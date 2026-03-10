1f377cba1a2ef1788f1c69322f843cadc4d43730
Jingrong Wei <rwei0744@gmail.com>
2026-03-10 22:17:56 +0000

    Student A Update: Completed all core database models (models.py) based on the ER diagram and configured the Admin backend.

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Ingredient, Inventory, Recipe, RecipeIngredient, Reminder
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Inventory)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(Reminder)
