622cd9a47973992b2fc54b457e9e8a768835da33
Jingrong Wei <rwei0744@gmail.com>
2026-03-12 23:24:30 +0000

    Complete the overall routing configuration for the project and successfully connect frontend and backend pages.

--- smartkitchen/urls.py
+++ smartkitchen/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import include, path
+from kitchen import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kitchen.urls')),
+
+
+    # 1. User Authentication Module
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    # 2. Other Page Routes
+    path('', views.home, name='home'),  # 默认打开的首页
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('recipes/', views.recipe_list_view, name='recipe_list'),
+    path('recipes/detail/', views.recipe_detail_view, name='recipe_detail'),
+    path('ingredient/delete/<int:item_id>/', views.delete_ingredient, name='delete_ingredient'),
+    path('recipe/mark_cooked/', views.mark_recipe_cooked, name='mark_recipe_cooked'),
 ]
