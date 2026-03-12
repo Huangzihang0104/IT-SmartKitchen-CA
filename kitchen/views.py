b62df433fa6dc996a79812226c6fb5dcb2bebada
Jingrong Wei <rwei0744@gmail.com>
2026-03-12 23:12:08 +0000

    Complete the backend logic for user authentication (M1) and integrate it with the frontend page paths.

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,17 +1,49 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+
+
+# 1. register
+def register_view(request):
+    # if users clicked "create"
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful! Welcome to SmartKitchen!")
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'kitchen/auth/register.html', {'form': form})
+
+# 2. LOGIN 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"Welcome, {username}! ")
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'kitchen/auth/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('login')
 
 
 def home(request):
     return render(request, 'kitchen/home.html')
 
-
-def login_view(request):
-    return render(request, 'kitchen/auth/login.html')
-
-
-def register_view(request):
-    return render(request, 'kitchen/auth/register.html')
 
 
 def dashboard_view(request):
@@ -36,3 +68,7 @@ def mark_recipe_cooked(request):
     if request.method == 'POST':
         return JsonResponse({'success': True, 'message': 'Recipe marked as cooked.'})
     return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
+
+
+
+
