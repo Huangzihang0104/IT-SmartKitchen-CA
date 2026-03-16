71deedb7107054fd72694614971290a471faa5f0
Jingrong Wei <rwei0744@gmail.com>
2026-03-16 21:59:51 +0000

    Completed M2/M3 inventory management CRUD operations, including smart ingredient addition, editing, and deletion features

--- smartkitchen/urls.py
+++ smartkitchen/urls.py
@@ -12,11 +12,15 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
-    # 2. Other Page Routes
-    path('', views.home, name='home'),  # 默认打开的首页
+    # 2. M2/M3 Inventory Management Core Routing
+    path('', views.home, name='home'),  # open the default home page
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('inventory/add/', views.add_inventory_view, name='add_inventory'), # Add Ingredients page
+    path('inventory/edit/<int:item_id>/', views.edit_inventory_view, name='edit_inventory'), # Edit Ingredients Page
+    path('ingredient/delete/<int:item_id>/', views.delete_ingredient, name='delete_ingredient'),
+
+    # Other page routes
     path('recipes/', views.recipe_list_view, name='recipe_list'),
     path('recipes/detail/', views.recipe_detail_view, name='recipe_detail'),
-    path('ingredient/delete/<int:item_id>/', views.delete_ingredient, name='delete_ingredient'),
     path('recipe/mark_cooked/', views.mark_recipe_cooked, name='mark_recipe_cooked'),
 ]
